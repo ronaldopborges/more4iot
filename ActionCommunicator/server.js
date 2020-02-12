03c36c55e3006e2aded9f56caef6ca149f4071c6
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-02-11 22:51:50 -0300

    ActionCommunicator: add config e dependencias

--- ActionCommunicator/server.js
+++ ActionCommunicator/server.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 const addresses = require('./config/addresses');
-const { DATABASE_URL } = require('./config/env');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors())
 server.use(routes);
-
 
 server.listen(addresses.actionCommunicatorPort, () => {
     console.log("Action Communicator online...")

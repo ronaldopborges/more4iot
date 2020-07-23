b65c108d799758ee8aad52b7493af10d3e5f8412
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-07-23 09:56:37 -0300

    change: start.js to server.js

---
+++ src/DeviceManager/model/device.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const device = require('uuid/v1');
+
+const DeviceSchema = new Schema(
+    {
+        uuid: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        latDefault: {
+            type: Number,
+            required: true,
+        },
+        lonDefault: {
+            type: Number,
+            required: true,
+        },
+        resource: [{
+            type: String
+        }],
+        uri: {
+            type: String,
+            required: true
+        },
+        protocol: {
+            type: String,
+            required: true
+        },
+        describe: {
+            type: String,
+            required: true
+        },
+        typeDevice: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+    });
+
+/**
+ * Generates an unique id for a new device
+ */
+const generateUuid = async () => {
+    const hash = await device();
+    return hash;
+};
+
+
+exports.db = model('Device', DeviceSchema);
+exports.generateUuid = generateUuid;
---
+++ src/DeviceManager/repository/DeviceRepository.js
@@ -0,0 +1,93 @@
+const Device = require('../model/device');
+
+/**
+ * Registers a new device into database
+ * @param  {Object} device The device object with all the attributes required to register into database
+ */
+const save = async device => {
+    try {
+        console.log(device)
+        const newDevice = await Device.db.create(device);
+        if (newDevice)
+            return newDevice;
+        else return false;
+    } catch (error) {
+        return false;
+    }
+}
+
+/**
+ * Updates the data of a device from database
+ * @param  {Object} deviceUpdated Object which contains the UUID and updated attribute(s) to update into database
+ */
+const update = async deviceUpdated => {
+    try {
+        const deviceExists = await Device.db.findOne({ uuid: deviceUpdated.uuid });
+        if (deviceExists) {
+            const device = await Device.db.findByIdAndUpdate(deviceExists._id, deviceUpdated, { new: true })
+            return device;
+        } else
+            return false;
+    } catch (error) {
+        return false;
+    }
+
+}
+
+/**
+ * Get the data of a device from database
+ * @param  {string} deviceUuid UUID of the target device to get
+ */
+const findByUuid = async deviceUuid => {
+    try {
+        const deviceExists = await Device.db.findOne({ uuid: deviceUuid });
+        if (deviceExists)
+            return deviceExists;
+        else
+            return false;
+    } catch (error) {
+        return false;
+    }
+
+}
+
+/**
+ * Returns all the registered devices from the database in an array of objects
+ */
+const findAll = async () => {
+    try {
+        const all = await Device.db.find({})
+        if (all)
+            return all;
+        else
+            return false;
+    } catch (error) {
+        return false;
+    }
+}
+
+/**
+ * Deletes a device from the database
+ * @param  {string} deviceUuid UUID of the target device to delete
+ */
+const deleteh = async deviceUuid => {
+    try {
+        const deviceExists = await Device.db.findOne({ uuid: deviceUuid });
+        if (deviceExists) {
+            await Device.db.findByIdAndRemove(deviceExists._id);
+            return true;
+        }
+        else
+            return false;
+    } catch (error) {
+        return false;
+    }
+}
+
+exports.delete = deleteh;
+exports.findAll = findAll;
+exports.findByUuid = findByUuid;
+exports.update = update;
+exports.save = save;
+
+

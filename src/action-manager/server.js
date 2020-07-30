18054127a60d283a438fc0f95b2521bb6494bb0d
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-07-30 11:52:11 -0300

    update: server.js, package.json

--- src/action-manager/server.js
+++ src/action-manager/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+const addresses = require('./config/addresses');
+const { DATABASE_URL } = require('./config/env');
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(routes);
+
+mongoose.connect(DATABASE_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+
+server.listen(addresses.actionManagerPort, () => {
+  console.log("Action manager online...")
+});
 
 module.exports = server

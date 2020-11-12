c09277ab18da1d09c8458e4da908fd2f0b7d0acc
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-11-12 15:41:08 -0300

    fix: swagger api and routes

--- src/device-manager/server.js
+++ src/device-manager/server.js
@@ -1,14 +1,10 @@
-const express   = require('express');
-const routes    = require('./routes');
-const cors      = require('cors');
-const server    = express();
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(routes);
-server.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerOptions)
-);
 
-module.exports  = server
+module.exports = server

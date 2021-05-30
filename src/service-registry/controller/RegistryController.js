c5b64b9feed6057e20a68386474b9ed1dd5b4a01
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2021-05-30 19:30:52 -0300

    fix: service registry route

--- src/service-registry/controller/RegistryController.js
+++ src/service-registry/controller/RegistryController.js
@@ -1,14 +1,20 @@
 const RegistryRepository = require('../repository/RegistryRepository');
+const debug = require('debug')('registry:controller');
 
 const inscribeRegistry = async (req, res) => {
+    debug(`registry service inscribe: ${req.body}`);
     response = await RegistryRepository.saveByServiceName(req.body);
+    debug(`registry service inscribe: ${response}`);
     return res.json(response);
 }
 
 const getRegistry = async (req, res) => {
+    debug(`registry service receiver request: ${req.params.name}`);
     const response = await RegistryRepository.findByServiceName(req.params.name);
-    if (response)
+    if (response) {
+        debug(`receiver request: ${req.params.name}`);
         return res.json(response);
+    }
     else return res.send(false);
 }
 
--- src/service-registry/routes.js
+++ src/service-registry/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = express.Router();
 
-const RegistryController = require('./controller/RegistryController');
+const { serviceRegistryRouteInscribe, serviceRegistryRouteGet } = require('@iotufersa/more4iot-js-sdk/config/routes');
+const controller = require('./controller/RegistryController');
 
-routes.post(`/`, RegistryController.inscribeRegistry);
-routes.get(`/:name`, RegistryController.getRegistry);
+routes.post(`/${serviceRegistryRouteInscribe}`, controller.inscribeRegistry);
+routes.get(`/${serviceRegistryRouteGet}/:name`, controller.getRegistry);
 
 module.exports = routes;
